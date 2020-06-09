b527ea31dac3d9a6e6f30a7a1e4d19280bd5917b
Suresh Pisupati <63001293+suresh-pisupati@users.noreply.github.com>
2020-06-09 17:20:01 +0530

    Java 9 Feature
    Private methods in interface

---
+++ Java 8/PrivateMethodsOfInterface.java
@@ -0,0 +1,12 @@
+package com.suresh.java891011features;
+
+public class PrivateMethodsOfInterface implements GreetingsInterface {
+
+	public static void main(String[] args) {
+
+		PrivateMethodsOfInterface obj = new PrivateMethodsOfInterface();
+		obj.familySMS();
+		obj.friendSMS();
+	}
+
+}
