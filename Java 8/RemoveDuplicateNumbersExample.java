72615ac7fe1d7ea1eca73d4072d45385df1a33e3
Suresh Pisupati <63001293+suresh-pisupati@users.noreply.github.com>
2020-06-12 18:30:35 +0530

    Update RemoveDuplicateNumbersExample.java
    Added optimized logic for removing the duplicates elements in the given array

--- Java 8/RemoveDuplicateNumbersExample.java
+++ Java 8/RemoveDuplicateNumbersExample.java
@@ -7,13 +7,13 @@ public class RemoveDuplicateNumbersExample {
    public static void main( String[] args ) {
 
       int numbers[] = { 1, 1, 2, 2, 3, 4, 5, 6 };
-      int[] requiredArray = removeDuplicates( numbers );
+      int[] requiredArray = removeDuplicates1( numbers );
       for( int n : requiredArray ) {
          System.out.println( n );
       }
    }
 
-   private static int[] removeDuplicates( int[] numbers ) {
+   private static int[] removeDuplicates1( int[] numbers ) {
 
       int count = numbers.length;
       for( int i = 0; i < count; i++ ) {
@@ -26,6 +26,24 @@ public class RemoveDuplicateNumbersExample {
          }
       }
       return Arrays.copyOf( numbers, count );
+   }
+   
+   //Time Complexity:   O(n)
+   //Space Complexity:  O(1)
+   private static int removeDuplicates2(int[] numbers) {
+    
+      int length = numbers.length();
+      if(length == 0 || length == 1) {
+         return length;
+      }
+      int j = 0;
+      for(int i = 0; i < length - 1; i++) {
+         if(numbers[i] != numbers[i+1]) {
+            numbers[j++] = numbers[i];
+         }
+      }
+      numbers[j++] = numbers[length - 1];
+      return j;
    }
 
 }
