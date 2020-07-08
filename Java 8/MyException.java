8dc0f95a4703918f7c2c651a3262a0cf25c54b5b
Suresh Pisupati <63001293+suresh-pisupati@users.noreply.github.com>
2020-07-08 14:51:56 +0530

    UnCaughtExceptionHandler

---
+++ Java 8/MyException.java
@@ -0,0 +1,30 @@
+package com.suresh.java891011features;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+public class MyException implements UncaughtExceptionHandler {
+
+	private String message;
+
+	public MyException() {
+
+	}
+
+	public MyException(String message) {
+
+		this.message = message;
+	}
+
+	@Override
+	public void uncaughtException(Thread t, Throwable e) {
+
+		System.out.println("Uncaught Exception occurred and catching here... " + e.getLocalizedMessage());
+	}
+
+	@Override
+	public String toString() {
+
+		return message;
+	}
+
+}
---
+++ Java 8/UnCaughtExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.suresh.java891011features;
+
+import java.util.List;
+
+public class UnCaughtExceptionHandler {
+
+	public UnCaughtExceptionHandler() {
+
+		NewThread newThread = new NewThread();
+		newThread.setName("New Thread");
+		newThread.setUncaughtExceptionHandler(new MyException());
+		newThread.start();
+	}
+
+	private class NewThread extends Thread {
+
+		@Override
+		public void run() {
+			try {
+				List<Integer> numbersList = null;
+				numbersList.add(1);
+			} catch (Exception ex) {
+				throw new NullPointerException("Object not initialized.");
+			}
+		}
+	}
+
+	public static void main(String[] args) throws CustomException {
+
+		new UnCaughtExceptionHandler();
+	}
+}
