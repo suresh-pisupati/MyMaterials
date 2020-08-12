4140ce142cb0d0ca7fcada88d8a397990361044c
pksuresh <pksuresh@PKSURESH-IN.idc.oracle.com>
2020-08-12 16:42:27 +0530

    Removing from Java Script branch

--- Java 8/com/suresh/maven/Interface2.java
+++
@@ -1,8 +0,0 @@
-package com.suresh.maven;
-
-public interface Interface2 {
-
-	default void printName(String name) {
-		System.out.println( "My Name is....... "  +name + "    in interface2");
-	}
-}
