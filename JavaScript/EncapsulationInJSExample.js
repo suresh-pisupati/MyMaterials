cd67e6d43972fb535a75fb723fea7bc4fb7264d4
pksuresh <pksuresh@PKSURESH-IN.idc.oracle.com>
2020-08-12 20:47:39 +0530

    Encapsulation example in JS

---
+++ JavaScript/EncapsulationInJSExample.js
@@ -0,0 +1,25 @@
+/**
+ *  This example demonstrates the encapsulation concept in java script.
+ *  
+ */
+function employee() {
+
+	var firstName = "suresh";
+	var lastName = "kumar";
+
+	var fullName = {
+		"getFirstName" : function() {
+			return firstName;
+		},
+
+		"getLastName" : function() {
+			return lastName;
+		},
+	};
+	return fullName;
+}
+
+var emp = employee();
+console.log("First Name: " + emp.getFirstName()); // Prints First Name: suresh
+console.log("Last Name:   " + emp.getLastName()); // Prints Last Name:   kumar
+console.log("First Name using var :   " + emp.firstName); // First Name using var :   undefined
