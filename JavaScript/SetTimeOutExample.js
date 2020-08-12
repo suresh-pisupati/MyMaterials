2059ed9298e5edd48e497526ddd104496df589aa
pksuresh <pksuresh@PKSURESH-IN.idc.oracle.com>
2020-08-12 16:40:14 +0530

    Example for setTimeout() and for making synchronous response

---
+++ JavaScript/SetTimeOutExample.js
@@ -0,0 +1,16 @@
+/**
+ * This is the example for making the java script call as synchronous call.
+ * Since java script calls are asynchronous calls, and setTimeout will be for a delay of second,
+ * if we do not use this approach, the last value will be printed 10 times. 
+ */
+var i;
+for (i = 1; i <= 10; i++) {
+	myFunction(i);
+}
+
+function myFunction(i)  {
+	var currentValue = i;
+	setTimeout(function() {
+		console.log(currentValue);
+	}, 1000);
+};
