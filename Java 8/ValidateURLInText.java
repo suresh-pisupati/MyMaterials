6bd80cf0406f7fc9dc1d9daf9199b99a7a92d287
pksuresh <pksuresh@PKSURESH-IN.idc.oracle.com>
2020-08-17 16:49:43 +0530

    Reformatting the code

--- Java 8/ValidateURLInText.java
+++ Java 8/ValidateURLInText.java
@@ -5,20 +5,21 @@ public class ValidateURLInText {
 
 	private static final String URL_REGEX = "^(http(s?))\\:\\/\\/[0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z])*(:(0-9)*)*(\\/?)([a-zA-Z0-9\\-\\.\\?\\,\\:\\'\\/\\\\\\+=&amp;%\\$#_]*)?$";
 	private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
-	
+
 	public static boolean validateURL(String url) {
-		if(url == null) {
+		if (url == null) {
 			return false;
 		}
-		if(!url.contains("://")) {
-			url = "http://" +url; 
+		if (!url.contains("://")) {
+			url = "http://" + url;
 		}
 		Matcher matcher = URL_PATTERN.matcher(url);
 		return matcher.matches();
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		String url = "http://www.oracle.com";
-		System.out.println( "url is                   "    +validateURL(url));
+		System.out.println("url is                   " + validateURL(url));
 	}
+}
