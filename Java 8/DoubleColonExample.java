1ee639aa52c97dfc2bc4acc4c2096fc979eb8e0e
pksuresh <pksuresh@PKSURESH-IN.idc.oracle.com>
2020-08-17 16:51:44 +0530

    Refactoring the code

--- Java 8/DoubleColonExample.java
+++ Java 8/DoubleColonExample.java
@@ -1,26 +1,26 @@
-package com.suresh.maven;
-
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * The double colon (::) operator, also known as method reference operator in Java, is used to call
- * a method by referring to it with the help of its class directly.
+ * The double colon (::) operator, also known as method reference operator in
+ * Java, is used to call a method by referring to it with the help of its class
+ * directly.
  */
 public class DoubleColonExample {
 
-   public static void main( String[] args ) {
+	public static void main(String[] args) {
 
-      List <Integer> list = new ArrayList <>();
-      list.add( 10 );
-      list.add( 20 );
-      list.add( 30 );
-      list.add( 40 );
-      list.add( 50 );
+		List<Integer> list = new ArrayList<>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(50);
 
-      // To print the elements in the list we can use lambda expressions instead
-      // method reference will be used.
-      list.forEach( System.out::println );
-   }
+		// To print the elements in the list we can use lambda expressions
+		// instead
+		// method reference will be used.
+		list.forEach(System.out::println);
+	}
 
 }
--- Java 8/EggDropProblem.java
+++ Java 8/EggDropProblem.java
@@ -1,38 +1,38 @@
-package com.org.suresh;
-
 public class EggDropProblem {
 
-   private static int findFloorsForEggDrop( int nEggs, int nFloors ) {
+	private static int findFloorsForEggDrop(int nEggs, int nFloors) {
 
-      // If there are no floors or only one Floor just return the number
-      if( nFloors == 0 || nFloors == 1 ) {
-         return nFloors;
-      }
+		// If there are no floors or only one Floor just return the number
+		if (nFloors == 0 || nFloors == 1) {
+			return nFloors;
+		}
 
-      // If we are left with only one egg, we need to try with all or remaining floors.
-      if( nEggs == 1 ) {
-         return nFloors;
-      }
+		// If we are left with only one egg, we need to try with all or
+		// remaining floors.
+		if (nEggs == 1) {
+			return nFloors;
+		}
 
-      int requiredFloors = Integer.MAX_VALUE;
-      int res;
-      /*
-       * 1) If the egg breaks after dropping from nth floor, then we only need to check for floors
-       * lower than x with remaining eggs; so the problem reduces to x-1 floors and n-1 eggs. 2) If
-       * the egg doesn’t break after dropping from the nth floor, then we only need to check for
-       * floors higher than x; so the problem reduces to nFloors-x floors and n eggs.
-       */
-      for( int i = 1; i <= nFloors; i++ ) {
-         res = Math.max( findFloorsForEggDrop( nEggs - 1, i - 1 ), findFloorsForEggDrop( nEggs, nFloors - i ) );
-         if( res < requiredFloors ) {
-            requiredFloors = res;
-         }
-      }
-      return requiredFloors + 1;
-   }
+		int requiredFloors = Integer.MAX_VALUE;
+		int res;
+		/*
+		 * 1) If the egg breaks after dropping from nth floor, then we only need
+		 * to check for floors lower than x with remaining eggs; so the problem
+		 * reduces to x-1 floors and n-1 eggs. 2) If the egg doesn’t break after
+		 * dropping from the nth floor, then we only need to check for floors
+		 * higher than x; so the problem reduces to nFloors-x floors and n eggs.
+		 */
+		for (int i = 1; i <= nFloors; i++) {
+			res = Math.max(findFloorsForEggDrop(nEggs - 1, i - 1), findFloorsForEggDrop(nEggs, nFloors - i));
+			if (res < requiredFloors) {
+				requiredFloors = res;
+			}
+		}
+		return requiredFloors + 1;
+	}
 
-   public static void main( String[] args ) {
+	public static void main(String[] args) {
 
-      System.out.println( findFloorsForEggDrop( 2, 10 ) );
-   }
+		System.out.println(findFloorsForEggDrop(2, 10));
+	}
 }
