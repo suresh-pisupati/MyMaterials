8c42380462d1a66a713b9e881bd00eafc032eb36
Suresh Pisupati <sureshsarma@gmail.com>
2020-10-28 00:31:04 +0530

    Two numbers sum using data structure pattern

---
+++ Java 8/TwoNumberSum.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoNumberSum {
+
+	// Using the data structure pattern
+	// TC: O(n)
+	// SC: O(n) //Worst case scenario
+	public static void getPair(int[] givenArray, int requiredSum) {
+
+		Set<Integer> set = new HashSet<>();
+		for (int index = 0; index < givenArray.length; index++) {
+			int element = requiredSum - givenArray[index];
+			if (set.contains(element)) {
+				System.out.println("Required indexes are      " + (givenArray[index]) + " , " + element);
+			}
+			set.add(givenArray[index]);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int[] givenArray = { 1, 5, 3, 2, 6, 9, 4 };
+		int requiredSum = 7;
+		getPair(givenArray, requiredSum);
+
+	}
+
+}
