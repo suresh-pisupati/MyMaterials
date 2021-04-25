c821293014b8e2e6f51d2d269a793e64f66e4e2a
Suresh Pisupati <sureshsarma@gmail.com>
2021-04-26 00:08:15 +0530

    Added the method logic as comments

--- Java 8/arrayproblems/MajorityOfElementProblem.java
+++ Java 8/arrayproblems/MajorityOfElementProblem.java
@@ -2,6 +2,18 @@ package arrayproblems;
 
 public class MajorityOfElementProblem {
 
+	// TC: O(n)
+	// SC: O(1)
+	/**
+	 * Logic: Initialize a max and count variables Iterate thru the given array
+	 * elements length and for each element check with max element. If the
+	 * element is equal to max element increment the count else decrement the
+	 * count. While decrementing the count if the count is equal to 0 then max
+	 * value will be the current element and count needs to be set to 1
+	 * 
+	 * @param givenArray
+	 * @return integer, majority element
+	 */
 	public static int majorityElement(int[] givenArray) {
 
 		int majorityElement = Integer.MAX_VALUE;
