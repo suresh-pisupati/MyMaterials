71c8e1ee76926cbceb58b3495211efda227301f6
Suresh Pisupati <sureshsarma@gmail.com>
2021-06-03 15:52:01 +0530

    Second Largest element in the given array

---
+++ Java 8/arrayproblems/SecondLargestElementInArray.java
@@ -0,0 +1,40 @@
+package arrayproblems;
+
+public class SecondLargestElementInArray {
+
+	/**
+	 * The logic is: <br>
+	 * 1. Declare two variables and initialize them to Integer.Min value. <br>
+	 * 2. Iterate through each element and <br>
+	 * i. if the current element is greater than firstMax element, store the
+	 * current firstMax value in the secondMax element and current element in
+	 * the firstMax variable<br>
+	 * ii. if the current element is in between secondMax and firstMax, update
+	 * secondMax to current value
+	 * 
+	 * @param givenArray
+	 * 			@return, an integer, which is second largest element in the
+	 *            given array
+	 */
+	private static int secondLargestElement(int[] givenArray) {
+
+		int firstMax = Integer.MIN_VALUE;
+		int secondMax = Integer.MIN_VALUE;
+		for (int element : givenArray) {
+			if (element > firstMax) {
+				secondMax = firstMax;
+				firstMax = element;
+			} else if (element > secondMax && element != firstMax) {
+				secondMax = element;
+			}
+		}
+		return secondMax;
+	}
+
+	public static void main(String[] args) {
+
+		int[] givenArray = { 12, 35, 15, 16 };
+		System.out.println(secondLargestElement(givenArray));
+	}
+
+}
