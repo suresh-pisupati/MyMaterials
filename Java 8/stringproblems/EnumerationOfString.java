7b8fb20230bd0f42b6996dec9bb6f9359846d638
Suresh Pisupati <sureshsarma@gmail.com>
2021-06-07 20:01:50 +0530

    Enumeration of all strings using length/position of each character in the string

---
+++ Java 8/stringproblems/EnumerationOfString.java
@@ -0,0 +1,19 @@
+package stringproblems;
+
+public class EnumerationOfString {
+
+	// TC: Theta(n^2) SC:Theta(n)
+	public static void enumerateAllStrings(String givenString) {
+
+		for (int i = 0; i < givenString.length(); ++i) {
+			for (int j = i; j <= givenString.length(); ++j) {
+				System.out.println(givenString.substring(i, j));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		enumerateAllStrings("abcd");
+	}
+
+}
