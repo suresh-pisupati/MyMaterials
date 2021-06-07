7b3a54d8fd77ea7e21bcf2ea5be7709f505903cc
Suresh Pisupati <sureshsarma@gmail.com>
2021-06-07 23:27:04 +0530

    Corrected the Space complexity which should be  O(1)

--- Java 8/stringproblems/EnumerationOfString.java
+++ Java 8/stringproblems/EnumerationOfString.java
@@ -2,7 +2,7 @@ package stringproblems;
 
 public class EnumerationOfString {
 
-	// TC: Theta(n^2) SC:Theta(n)
+	// TC: Theta(n^2) SC:Theta(1)
 	public static void enumerateAllStrings(String givenString) {
 
 		for (int i = 0; i < givenString.length(); ++i) {
