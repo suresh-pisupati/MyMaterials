78958ac8481afa711b43d92d5ebb8dc5067dd803
Suresh Pisupati <sureshsarma@gmail.com>
2021-06-09 01:23:37 +0530

    Added documentation for the class

--- Java 8/stringproblems/AllSubSetsOfString.java
+++ Java 8/stringproblems/AllSubSetsOfString.java
@@ -1,5 +1,24 @@
 package stringproblems;
 
+/**
+ * Subsets of given string:<br>
+ * A sub-set is possible strings of a given string without considering the
+ * sequence of characters in it. The difference between Enumeration of the
+ * String and SubSets of given string is, in the enumeration sequence will not
+ * be missed. Where as in case of subsets, the order will be missing.<br>
+ * For ex: for the given String "abc"<br>
+ * sub strings are:<br>
+ * a, ab, abc, ac, b, bc, c<br>
+ * In case of Enumeration of the above string 'ac' will not be in the result.
+ * <p>
+ * The idea to solve the sub string is calling a recursive function where the
+ * each character is included in one iteration and excluded in another
+ * iteration.
+ * </p>
+ * 
+ * @author pksuresh
+ *
+ */
 public class AllSubSetsOfString {
 
 	public static void allSubsets1(String in) {
@@ -7,13 +26,25 @@ public class AllSubSetsOfString {
 		auxAllSubsets1(0, in, "");
 	}
 
+	/**
+	 * Initially the required index will be from zero position. Increment the
+	 * position till the length of the string. While incrementing the index, at
+	 * the first step include the character in one iteration and in another
+	 * iteration exclude that character.
+	 * 
+	 * @param d
+	 * @param in
+	 * @param res
+	 */
 	private static void auxAllSubsets1(int d, String in, String res) {
 
 		if (d == in.length()) {
 			System.out.println(res);
 			return;
 		}
+		// Include the character
 		auxAllSubsets1(d + 1, in, res + in.charAt(d));
+		// Exclude the character
 		auxAllSubsets1(d + 1, in, res + "");
 	}
 
@@ -33,7 +64,7 @@ public class AllSubSetsOfString {
 
 	public static void main(String[] args) {
 
-		allSubsets1("abc");
+		allSubsets2("abc");
 	}
 
 }
