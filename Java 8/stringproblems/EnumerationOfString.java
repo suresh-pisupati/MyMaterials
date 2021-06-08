62789cd98a94f8016d3b3686d4a4788d5efb3e82
Suresh Pisupati <sureshsarma@gmail.com>
2021-06-09 00:47:48 +0530

    Added Documentation of the class

--- Java 8/stringproblems/EnumerationOfString.java
+++ Java 8/stringproblems/EnumerationOfString.java
@@ -1,5 +1,17 @@
 package stringproblems;
 
+/**
+ * Enumeration of given string Enumeration is extracting all the possible sub
+ * strings starting from the single character without missing the order.<br>
+ * For ex: for the given String "abc" enumerated strings are: a, ab, abc, b, bc,
+ * c<br>
+ * The logic to solve this problem is to iterate through each character of the
+ * string by taking two loops where the second loop will traverse for the sub
+ * strings with each character in the first loop.
+ * 
+ * @author pksuresh
+ *
+ */
 public class EnumerationOfString {
 
 	// TC: Theta(n^2) SC:Theta(1)
@@ -13,7 +25,7 @@ public class EnumerationOfString {
 	}
 
 	public static void main(String[] args) {
-		enumerateAllStrings("abcd");
+		enumerateAllStrings("abc");
 	}
 
 }
