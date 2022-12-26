6bafceb9b1e253576ab723f831d3ba414a0db7e4
suresh.pisupati <sureshsarma@gmail.com>
2022-12-25 20:17:05 -0600

    Contains only String related problems

--- Java 8/Interface1.java
+++
@@ -1,9 +0,0 @@
-
-public interface Interface1 {
-
-	public void display();
-	
-	default void displayName(String name) {
-		System.out.println( "In the interface 1:::::     "  +name);
-	}
-}
--- Java 8/Interface2.java
+++
@@ -1,7 +0,0 @@
-
-public interface Interface2 {
-	
-	default void displayName(String name) {
-		System.out.println( "In the interface 2:::::     "  +name);
-	}
-}
